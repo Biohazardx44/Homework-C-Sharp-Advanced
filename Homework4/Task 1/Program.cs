c30aa3d1b1ca9d3bf38050ff829254a9d9a45bb2
Biohazardx44 <nikolailievski420@gmail.com>
2023-04-21 20:19:25 +0200

    Homework 4 Minor Adjustments

--- Homework4/Task 1/Program.cs
+++ Homework4/Task 1/Program.cs
@@ -6,15 +6,9 @@ List<Person> sortedList = new List<Person>();
 
 Console.WriteLine("All people starting with 'R', ordered by Age - (DESCENDING ORDER):");
 
-foreach (Person person in peopleList)
-{
-    if (person.FirstName.StartsWith("R"))
-    {
-        filteredList.Add(person);
-    }
-}
-
-sortedList = filteredList.OrderByDescending(p => p.Age).ToList();
+sortedList = peopleList.Where(person => person.FirstName.StartsWith("R"))
+                       .OrderByDescending(person => person.Age)
+                       .ToList();
 
 foreach (Person person in sortedList)
 {
@@ -26,15 +20,9 @@ Console.WriteLine("\nAll brown dogs older than 3 years, ordered by Age - (ASCEND
 List<Dog> brownDogs = new List<Dog>();
 List<Dog> dogList = Database.Dogs;
 
-foreach (Dog dog in dogList)
-{
-    if (dog.Color == "Brown" && dog.Age > 3)
-    {
-        brownDogs.Add(dog);
-    }
-}
-
-List<Dog> sortedBrownDogs = brownDogs.OrderBy(d => d.Age).ToList();
+List<Dog> sortedBrownDogs = dogList.Where(dog => dog.Color == "Brown" && dog.Age > 3)
+                                   .OrderBy(dog => dog.Age)
+                                   .ToList();
 
 foreach (Dog dog in sortedBrownDogs)
 {
@@ -44,15 +32,9 @@ foreach (Dog dog in sortedBrownDogs)
 Console.WriteLine("\nAll people with more than 2 dogs, ordered by Name - (DESCENDING ORDER):");
 List<Person> peopleWithMoreThanTwoDogs = new List<Person>();
 
-foreach (Person person in peopleList)
-{
-    if (person.Dogs.Count > 2)
-    {
-        peopleWithMoreThanTwoDogs.Add(person);
-    }
-}
-
-List<Person> sortedPeopleWithMoreThanTwoDogs = peopleWithMoreThanTwoDogs.OrderByDescending(p => p.FirstName).ToList();
+List<Person> sortedPeopleWithMoreThanTwoDogs = peopleList.Where(person => person.Dogs.Count > 2)
+                                                         .OrderByDescending(person => person.FirstName)
+                                                         .ToList();
 
 foreach (Person person in sortedPeopleWithMoreThanTwoDogs)
 {
@@ -71,8 +53,8 @@ foreach (Dog dog in freddysDogs)
 }
 
 Dog? nathensFirstDog = peopleList.Where(p => p.FirstName == "Nathen")
-                                     .SelectMany(p => p.Dogs)
-                                     .FirstOrDefault();
+                                 .SelectMany(p => p.Dogs)
+                                 .FirstOrDefault();
 
 if (nathensFirstDog != null)
 {
@@ -85,11 +67,11 @@ else
 
 Console.WriteLine("\nAll white dogs names from Cristofer, Freddy, Erin and Amelia, ordered by Name - (ASCENDING ORDER):");
 List<string> whiteDogsNames = peopleList.Where(p => new[] { "Cristofer", "Freddy", "Erin", "Amelia" }.Contains(p.FirstName))
-                                             .SelectMany(p => p.Dogs)
-                                             .Where(d => d.Color == "White")
-                                             .OrderBy(d => d.Name)
-                                             .Select(d => d.Name)
-                                             .ToList();
+                                        .SelectMany(p => p.Dogs)
+                                        .Where(d => d.Color == "White")
+                                        .OrderBy(d => d.Name)
+                                        .Select(d => d.Name)
+                                        .ToList();
 
 foreach (string name in whiteDogsNames)
 {
