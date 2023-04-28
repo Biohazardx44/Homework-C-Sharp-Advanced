360bd0f590f7923eab7fe4dec520846b451ad000
Biohazardx44 <nikolailievski420@gmail.com>
2023-04-28 17:19:50 +0200

    Newtonsoft.Json

---
+++ Homework5/Task1/Program.cs
@@ -0,0 +1,76 @@
+﻿using Newtonsoft.Json;
+using Task1.Entities;
+
+string folderPath = "../../../OurData";
+string filePath = folderPath + "/ourFile.json";
+
+if (!Directory.Exists(folderPath))
+{
+    Directory.CreateDirectory(folderPath);
+}
+
+if (!File.Exists(filePath))
+{
+    File.Create(filePath).Close();
+}
+
+void WriteToJson(string json)
+{
+    using (StreamWriter sw = new StreamWriter(filePath, true))
+    {
+        sw.WriteLine(json);
+    }
+}
+
+void ReadFromJson()
+{
+    using (StreamReader sr = new StreamReader(filePath))
+    {
+        Console.WriteLine(sr.ReadToEnd());
+    }
+}
+
+void PrintFromJson()
+{
+    using (StreamReader sr = new StreamReader(filePath))
+    {
+        while (!sr.EndOfStream)
+        {
+            string line = sr.ReadLine();
+            List<Dog> dogs = JsonConvert.DeserializeObject<List<Dog>>(line);
+
+            foreach (Dog dog in dogs)
+            {
+                Console.WriteLine($"Name: {dog.Name}, Age: {dog.Age}, Color: {dog.Color}");
+            }
+        }
+    }
+}
+
+Console.WriteLine("Enter dog name:");
+string name = Console.ReadLine();
+
+Console.WriteLine("Enter dog age:");
+int age = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter dog color:");
+string color = Console.ReadLine();
+
+List<Dog> dogList = new List<Dog>();
+
+Dog newDog = new Dog
+{
+    Name = name,
+    Age = age,
+    Color = color
+};
+
+dogList.Add(newDog);
+
+string Serialized = JsonConvert.SerializeObject(dogList);
+
+WriteToJson(Serialized);
+Console.WriteLine("\nJSON FILE:");
+ReadFromJson();
+Console.WriteLine("All Dogs:");
+PrintFromJson();
